da0aceec2704b887a8eb49ea01c8ce8c6f9f9c6e
JAN <91562166+Kocjte@users.noreply.github.com>
2025-08-06 09:19:19 +0200

    Create data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,53 @@
+import google.colab
+
+def load_data(env="local"):
+    if env == "colab":
+        competition_name = "trojan-horse-hunt-in-space"
+
+        from google.colab import drive
+        drive.mount("/content/drive")
+
+        kaggle_creds_path = "/content/drive/MyDrive/kaggle.json"
+
+        ! pip install kaggle --quiet
+
+        ! mkdir ~/.kaggle
+        ! cp "/content/drive/MyDrive/kaggle.json" ~/.kaggle/
+        ! chmod 600 ~/.kaggle/kaggle.json
+        
+        ! kaggle competitions download -c {competition_name}
+        
+        ! mkdir kaggle_data
+        ! unzip {competition_name + ".zip"} -d kaggle_data
+        
+        drive.flush_and_unmount()
+
+    elif env == "kaggle":
+        # Read the training CSV into a DataFrame
+        train_data_df = pd.read_csv(
+            "/kaggle/input/trojan-horse-hunt-in-space/clean_train_data.csv",
+            index_col='id'
+        ).astype(np.float32)
+        
+        # Read the 45 models; note that model_id starts at 1.
+        
+        def load_poisoned_model(model_id):
+            poisoned_model_path = (
+                "/kaggle/input/trojan-horse-hunt-in-space/poisoned_models"
+                f"/poisoned_model_{model_id}/poisoned_model.pt"
+            )
+            poisoned_model = NHiTSModel.load(poisoned_model_path)
+            return poisoned_model
+
+        poisoned_model = [None]
+        for model_id in range(1, 46):
+            poisoned_model.append(load_poisoned_model(model_id))
+
+    elif env == "local":
+        data_path = "data/data.csv"
+
+    else:
+        raise ValueError(f"Unknown environment: {env}")
+
+    print(f"Loading data from {data_path}")
+
